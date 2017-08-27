2f7e9b4bfde9858d67321289fac0771e808db7e2
Jordan Scales <scalesjordan@gmail.com>
2017-08-27 10:40:43 -0400

    add FSM representing HAHA regex

--- src/finite_state_machine_test.ml
+++ src/finite_state_machine_test.ml
@@ -26,3 +26,31 @@ module Turnstile : Finite_state_machine.S = struct
     assert (Unlocked = List.fold_left transition Locked [InsertCoin]);
     assert (Locked = List.fold_left transition Locked [InsertCoin; Push; Push]);
 end
+
+module HAPlusRegex : Finite_state_machine.S = struct
+  type state = Start | H | Success | Failure
+  type symbol = char
+
+  let transition state symbol = match (state, symbol) with
+    | (Start, 'H') -> H
+    | (Start, _) -> Failure
+
+    | (H, 'A') -> Success
+    | (H, _) -> Failure
+
+    | (Success, 'H') -> H
+    | (Success, _) -> Failure
+
+    | (Failure, _) -> Failure
+
+  let () =
+    assert (Success = List.fold_left transition Start ['H'; 'A']);
+    assert (Success = List.fold_left transition Start ['H'; 'A'; 'H'; 'A']);
+    assert (Success = List.fold_left transition Start ['H'; 'A'; 'H'; 'A'; 'H'; 'A']);
+
+    assert (Success != List.fold_left transition Start ['K']);
+    assert (Success != List.fold_left transition Start ['H'; 'A'; 'Z']);
+    assert (Success != List.fold_left transition Start ['H'; 'A'; 'H']);
+    assert (Success != List.fold_left transition Start ['T'; 'H'; 'A']);
+    assert (Success != List.fold_left transition Start ['H'; 'A'; 'H'; 'A'; 'T']);
+end
